d4c2177916b07a53f3c32d6c6bf8dfaf3673df7f
Santosh-7124 <sunny962002@gmail.com>
2025-02-11 14:45:45 +0530

    vcf file added

--- src/SaveContactButton.jsx
+++ src/SaveContactButton.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 
-const handleSaveContact = async () => {
-    if ("contacts" in navigator && "write" in navigator.contacts) {
-      try {
-        const contact = {
-          name: ["Santosh"],
-          tel: ["+919347744815"],
-          email: ["sunny962002@gmail.com"],
-        };
-  
-        await navigator.contacts.write([contact]);
-        alert("Contact saved!");
-      } catch (error) {
-        console.error("Error saving contact:", error);
-      }
-    } else {
-      alert("Contact API not supported on this device.");
-    }
-  };
-  
-  const SaveContactButton = () => {
-    return <button onClick={handleSaveContact}>Save Contact</button>;
-  };
-  
-  export default SaveContactButton;
+const SaveContactButton = () => {
+  return (
+    <a href="/contact.vcf" download="contact.vcf">
+      <button>Save Contact</button>
+    </a>
+  );
+};
+
+export default SaveContactButton;
+
   
